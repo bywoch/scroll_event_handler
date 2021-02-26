4bf257f3c4d54de981433e8631008aee8cdccb77
Bright Chung <bywoch@gmail.com>
2021-02-26 17:00:00 +0900

    update js

--- js/common.js
+++ js/common.js
@@ -1,50 +1,36 @@
 $(document).ready(function () {
+    
+    // 배경 이미지 정보를 배열로 저장
+    var bgImages = [
+        { selector: '.edu_01', attachment: 'fixed', position: 'center -100px', size: '1920px 766px', offset: 100 },
+        { selector: '.edu_02', attachment: 'fixed', position: 'center -520px', size: '1920px 1408px', offset: 1287 },
+        { selector: '.edu_03', attachment: 'fixed', position: 'center -363px', size: '1920px 1259px', offset: 2545 },
+        { selector: '.edu_04', attachment: 'fixed', position: 'center -600px', size: '1920px 1487px', offset: 4032 },
+        { selector: '.edu_05', attachment: 'fixed', position: 'center -454px', size: '1920px 1343px', offset: 5376 },
+        { selector: '.edu_06', attachment: 'fixed', position: 'center -1043px', size: '1920px 1929px', offset: 7305 },
+        { selector: '.edu_07', attachment: 'fixed', position: 'center -92px', size: '1920px 978px', offset: 8282 },
+    ];
 
-    // edu Scroll Event
     $(window).scroll(function () {
         var scr = $(window).scrollTop();
 
-        if (scr > 100) {
-            $('.edu_01').css('background-attachment', 'fixed').css('background-position', 'center -100px').css('background-size', '1920px 766px');
-        } else {
-            $('.edu_01').css('background-attachment', 'inherit').css('background-position', 'center').css('background-size', 'cover');;
-        }
-
-        if (scr > 1287) {
-            $('.edu_02').css('background-attachment', 'fixed').css('background-position', 'center -520px').css('background-size', '1920px 1408px');
-        } else {
-            $('.edu_02').css('background-attachment', 'inherit').css('background-position', 'center').css('background-size', 'cover');;
-        }
-
-        if (scr > 2545) {
-            $('.edu_03').css('background-attachment', 'fixed').css('background-position', 'center -363px').css('background-size', '1920px 1259px');
-        } else {
-            $('.edu_03').css('background-attachment', 'inherit').css('background-position', 'center').css('background-size', 'cover');;
-        }
-
-        if (scr > 4032) {
-            $('.edu_04').css('background-attachment', 'fixed').css('background-position', 'center -600px').css('background-size', '1920px 1487px');
-        } else {
-            $('.edu_04').css('background-attachment', 'inherit').css('background-position', 'center').css('background-size', 'cover');;
-        }
-
-        if (scr > 5376) {
-            $('.edu_05').css('background-attachment', 'fixed').css('background-position', 'center -454px').css('background-size', '1920px 1343px');
-        } else {
-            $('.edu_05').css('background-attachment', 'inherit').css('background-position', 'center').css('background-size', 'cover');;
-        }
-
-        if (scr > 7305) {
-            $('.edu_06').css('background-attachment', 'fixed').css('background-position', 'center -1043px').css('background-size', '1920px 1929px');
-        } else {
-            $('.edu_06').css('background-attachment', 'inherit').css('background-position', 'center').css('background-size', 'cover');;
-        }
-
-        if (scr > 8282) {
-            $('.edu_07').css('background-attachment', 'fixed').css('background-position', 'center -92px').css('background-size', '1920px 978px');
-        } else {
-            $('.edu_07').css('background-attachment', 'inherit').css('background-position', 'center').css('background-size', 'cover');;
-        }
-
+        // 각 배경 이미지 정보를 순회하면서 처리
+        bgImages.forEach(function (image) {
+            var $element = $(image.selector);
+            if (scr > image.offset) {
+                $element.css({
+                    'background-attachment': image.attachment,
+                    'background-position': image.position,
+                    'background-size': image.size
+                });
+            } else {
+                $element.css({
+                    'background-attachment': 'inherit',
+                    'background-position': 'center',
+                    'background-size': 'cover'
+                });
+            }
+        });
     });
+
 });
